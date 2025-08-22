5c7f3e0f40b94a0567b30787c5350f15fcd73627
sanjeet2610 <195545915+sanjeet2610@users.noreply.github.com>
2025-08-22 15:56:04 +0000

    Add computer selection

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 100);
+    choice = choice % 3;
+    if (choice == 0) return "rock";
+    else if (choice == 1) return "paper";
+    else return "scissor";
+}
+
+const computerSelection = getComputerChoice();
